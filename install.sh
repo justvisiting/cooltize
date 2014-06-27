980a97c216317501b4c3306048a43945a90d34d6
justvisiting <hiteshchaudhary@gmail.com>
2014-06-27 14:43:04 -0700

    fixed minor issue in index.html header not showing
    fixed minor issue in index.html header not showing

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ sudo stop /cooltiz-webapp-nodejs
 curl -u 'justvisiting' -L -o master.zip  https://github.com/justvisiting/cooltize/zipball/master/
 unzip master.zip -d tmp
 cd tmp/*/webapp/
-sudo cp /cooltiz-webapp-nodejs.conf /etc/init
+sudo cp cooltize-webapp-nodejs.conf /etc/init
 cp -r ./* ~/cooltize/webapp/
-sudo start /cooltiz-webapp-nodejs
+sudo start cooltize-webapp-nodejs
