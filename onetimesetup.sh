ab9e1282d0d096a2a308aaa9c0dac16839247fba
justvisiting <hiteshchaudhary@gmail.com>
2014-07-01 14:42:49 -0700

    fixed button size issue on userform and contact card on contacts.html

--- onetimesetup.sh
+++ onetimesetup.sh
@@ -1,5 +1,6 @@
-sudo apt-get install nodejs
+sudo apt-get install nodejs unzip
 sudo apt-get update
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 mkdir -p ~/cooltize/webapp
 sudo sh -c 'cat cooltizeUIlogrotate.txt >> /etc/logrotate.conf'
+mkdir tmp
